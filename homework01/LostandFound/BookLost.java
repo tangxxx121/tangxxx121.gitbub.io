0111e49bf3daf86f7dfaa6d854a5d8125fd98292
tangxxx121 <3023242450@qq.com>
2022-07-17 19:02:46 +0800

    homework01

---
+++ homework01/LostandFound/BookLost.java
@@ -0,0 +1,6 @@
+package homework01.LostandFound;
+
+public class BookLost extends Lost{
+    public BookLost() {}
+
+}
---
+++ homework01/LostandFound/CardLost.java
@@ -0,0 +1,10 @@
+package homework01.LostandFound;
+
+public class CardLost extends Lost{
+    public CardLost(){}
+
+    //卡号
+    public String Num;
+    //学院信息
+    public String College;
+}
