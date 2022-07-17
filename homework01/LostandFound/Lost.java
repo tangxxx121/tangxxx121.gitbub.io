474223ed75c8fb128b9a3e6bc99764e1375d31c2
tangxxx121 <3023242450@qq.com>
2022-07-17 18:55:31 +0800

    homework01

---
+++ homework01/LostandFound/Lost.java
@@ -0,0 +1,13 @@
+package homework01.LostandFound;
+
+public class Lost {
+    public Lost(){}
+
+    //丢失的地点
+    public String name;
+    public String Pos_Lost;
+    //丢失的时间
+    public int Time_Lost;
+    //失物领取地点；
+    public String Pos_Found = "Lost Property Office";
+}
